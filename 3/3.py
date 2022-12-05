68983bdb118bde9efaa8f6e5fb63c8d8133027f7
sympataq <52965903+sympataq@users.noreply.github.com>
2022-12-05 16:55:29 +0100

    3.1 success

--- 3/3.py
+++ 3/3.py
@@ -1,6 +1,7 @@
 import sys
 
 file_data = []
+
 # priority = {num : val for num in range (1, 53) for val in range()}
 def read_file(filename='input'):
     filedata = []
@@ -12,10 +13,17 @@ def read_file(filename='input'):
         print("Wrong file or file path")
     return filedata
 
-def parse_data():
-    test = []
-    for num in range (150):
-        print(f'num {num}: {chr(num)}')
+def parse_data1(data):
+    res = []
+    for line in data:
+        comp1 = line[:len(line) // 2]
+        comp2 = line[len(line) // 2:]
+        for char in comp1:
+            if char in comp2:
+                res.append(char)
+                break
+    print(res)
+    return res        
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
@@ -23,7 +31,14 @@ if __name__ == '__main__':
     else:
         file_data = read_file('3/input')
 
-    # print(file_data)
-    parse_data()
+    score = 0
+    for char in parse_data1(file_data):
+        if ord(char) > 96: # a -> 97
+            score += ord(char) - 96
+        else:
+            score += ord(char)- 64 + 26 # A -> 65
+    print(score)
+
+
 
     
