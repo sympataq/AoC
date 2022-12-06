c8c1ee0d02bdb4e09e5e44c94a2087a3274ef8ce
sympataq <52965903+sympataq@users.noreply.github.com>
2022-12-06 17:37:30 +0100

    4.2 done

--- 4/4.py
+++ 4/4.py
@@ -1,6 +1,7 @@
-import sys, pathlib
+import sys, pathlib, re
 
 file_data =[]
+data = []
 
 
 def read_file(filename='input'):
@@ -13,7 +14,30 @@ def read_file(filename='input'):
         print("Wrong file or filepath")
     return filedata    
     
+def parse_file(data):
+        pars_data = []
+        pattern = re.compile(r'[0-9]+')
+        for line in data:
+            go = pattern.findall(line)
+            intList = [int(i) for i in go]
+            pars_data.append(intList)
+        return pars_data    
 
+def check_in_interval(line):
+    if (line[0] <= line[2] and line[1] >= line[3]) or \
+       (line[0] >= line[2] and line[1] <= line[3]):
+        return True
+    else:
+        return False
+
+def check_overlap (line):
+    if (line[0] >= line[2] and line[0] <= line[3]) or \
+       (line[1] >= line[2] and line[1] <= line[3]) or \
+       (line[2] >= line[0] and line[2] <= line[1]) or \
+       (line[3] >= line[0] and line[3] <= line[1]):
+        return True
+    else:
+        return False
 
 
 if __name__ == '__main__':
@@ -22,4 +46,10 @@ if __name__ == '__main__':
     else:
         file_data = read_file("4/input.txt")
 
-    print(file_data)
+    data = parse_file(file_data)
+
+    score,score2 = 0,0
+    for line in data:
+        score += check_in_interval(line)
+        score2 += check_overlap(line)
+    print(f"score 1:{score}, score 2:{score2}")
