746306cdab9b56e01d84d477993930ee9fefe809
sympataq <52965903+sympataq@users.noreply.github.com>
2022-12-07 05:38:11 +0100

    update

--- 6/6.py
+++ 6/6.py
@@ -6,16 +6,16 @@ file_data = []
 def read_file(filename='input.txt'):
     try:
         with open(filename) as f:
-           filedata = f.read()
+            filedata = f.read()
     except FileNotFoundError:
-        print("Wrong filename or path")                
+        print("Wrong filename or path")
     return filedata  
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         file_data = read_file(sys.argv[1])
     else:
-        file_data = read_file("6/input.txt")     
+        file_data = read_file("6/input.txt")    
 
     cd = Comm_device(file_data)
     print(cd.detect_packet(4))
