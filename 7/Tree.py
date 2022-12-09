9408c91f24baa230851ee06e042427cca5dc6525
sympataq <52965903+sympataq@users.noreply.github.com>
2022-12-09 04:02:58 +0100

    test

--- 7/Tree.py
+++ 7/Tree.py
@@ -1,8 +1,9 @@
 from Node import Node
 
+
 class Tree
 
-    def __init__(self, root:Node ):
+   def __init__(self, root: Node):
         self.root = root
 
     def traverse(self, root='root') -> str:
--- 8/8.py
+++ 8/8.py
@@ -1,42 +1,46 @@
 
 file_data = []
 
-def isVisibleVertical(data, row , column) -> bool:
+
+def isVisibleVertical(data, row, column) -> bool:
     # border
     if row == 0 or row == (len(data)-1):
         return True
 
-    #make "column"
-    col = [data[x][column] for x in range(len(data))] 
-    
+    # make "column"
+    col = [data[x][column] for x in range(len(data))]
+
     if max(col[0:row], default=0) < data[row][column]:
         return True
     if max(col[row+1:], default=0) < data[row][column]:
         return True
-    return False                
+    return False
 
-def isVisibleHorizontal(data, row , column) -> bool:
+
+def isVisibleHorizontal(data, row, column) -> bool:
     # border
-    if  column == 0 or column == len(data[row])-1:
+    if column == 0 or column == len(data[row])-1:
         return True
-    #inside
+    # inside
     if max(data[row][0:column], default=0) < data[row][column]:
         return True
     if max(data[row][column+1:], default=0) < data[row][column]:
         return True
-    return False                
+    return False
 
-def look_up(data, row , column) -> int:
-    if  row == 0:
+
+def look_up(data, row, column) -> int:
+    if row == 0:
         return 0
     # look up on column from actual row position
-    for look in range(row-1,0,-1):
+    for look in range(row-1, 0, -1):
         if data[look][column] >= data[row][column]:
             return row-look
     return row
 
-def look_down(data, row , column) -> int:
-    if  row == len(data)-1:
+
+def look_down(data, row, column) -> int:
+    if row == len(data)-1:
         return 0
     # look down on column from actual row position
     for look in range(row+1, len(data)-1):
@@ -44,16 +48,18 @@ def look_down(data, row , column) -> int:
             return look-row
     return len(data)-1-row
 
-def look_right(data, row , column) -> int:
-    if  column == len(data[row])-1:
+
+def look_right(data, row, column) -> int:
+    if column == len(data[row])-1:
         return 0
     # look right from actual column position
     for look in range(column+1, len(data[row])):
         if data[row][look] >= data[row][column]:
             return look-column
     return len(data[row])-1-column
-    
-def look_left(data, row , column) -> int:
+
+
+def look_left(data, row, column) -> int:
     if column == 0:
         return 0
     for look in range(column-1, 0, -1):
@@ -61,11 +67,12 @@ def look_left(data, row , column) -> int:
             return column - look
     return column
 
+
 def look_all_direction(data, row, column):
-    return  look_down(data, row, column) * \
-            look_up(data, row, column) * \
-            look_left(data, row, column) * \
-            look_right(data, row, column)
+    return look_down(data, row, column) * \
+        look_up(data, row, column) * \
+        look_left(data, row, column) * \
+        look_right(data, row, column)
 
 
 def file_read(filename='input.txt'):
@@ -79,23 +86,22 @@ def file_read(filename='input.txt'):
         print("Wrong filename or path")
     return filedata
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     file_data = file_read("8/input.txt")
 
 visible = 0
-for row in range (len(file_data)):
-    for column in range (len(file_data[row])):
+for row in range(len(file_data)):
+    for column in range(len(file_data[row])):
         if isVisibleHorizontal(file_data, row, column) or \
            isVisibleVertical(file_data, row, column):
             visible += 1
 print(visible)
 
-max_score, actual_score = 0,0
-for row in range (len(file_data)):
-    for column in range (len(file_data[row])):
-            actual_score = look_all_direction(file_data, row, column)
-            if  actual_score > max_score:
-                max_score = actual_score
+max_score, actual_score = 0, 0
+for row in range(len(file_data)):
+    for column in range(len(file_data[row])):
+        actual_score = look_all_direction(file_data, row, column)
+        if actual_score > max_score:
+            max_score = actual_score
 print(max_score)
-                 
-
