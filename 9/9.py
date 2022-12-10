262dffc157bfae8e8b3d3f2134d01ad48840cf49
sympataq <52965903+sympataq@users.noreply.github.com>
2022-12-10 06:00:40 +0100

    9.2 update

--- 9/9.py
+++ 9/9.py
@@ -1,4 +1,24 @@
 from worm import Worm
 
-w = Worm()
-print(w.head_tail_distance((-2, 3), (6, 4)))
+
+def read_file(filename):
+    data = []
+    with open(filename) as f:
+        for line in f:
+            line.rstrip()
+            direction, movement = line.split()
+            data.append([direction, int(movement)])
+    return data
+
+
+if __name__ == '__main__':
+    file_data = read_file("9/input.txt")
+
+    worm = Worm()
+    worm.movement_part1(file_data)
+    print(f'tail visited:{worm.tail_visited()}')
+
+    # add 9 body pieces
+    worm2 = Worm(9)
+    worm2.movement_part2(file_data, 7)
+    print(f'tail visited:{worm2.body_visited()}')
--- 9/worm.py
+++ 9/worm.py
@@ -9,11 +9,21 @@ class Worm:
     START_CHAR = 's'
     VISITED = '#'
 
-    def __init__(self):
+    def __init__(self, bodyparts=0):
         self.head_position = (0, 0)
         self.tail_position = (0, 0)
         self.head_movement = deque()
         self.tail_movement = deque()
+        self.head_movement.append(self.head_position)
+        self.tail_movement.append(self.tail_position)
+
+        # part 2
+        self.bodyparts = bodyparts
+        self.body_positions = []
+        self.body_movements = []
+        for i in range(bodyparts):
+            self.body_positions.append((0, 0))
+            self.body_movements.append(deque(self.body_positions[i]))
 
     def head_move_down(self):
         self.head_position = (self.head_position[0]-1, self.head_position[1])
@@ -33,39 +43,80 @@ class Worm:
 
     def tail_move(self):
         ''' move tail when head distance >= 2'''
-        if self.head_tail_distance() >= 2:
+        if self.head_tail_distance(self.head_position, self.tail_position) >= 2:
             # go to the same position when head was 1 step ago
-            self.tail_position = self.head_movement.index(-2)
+            self.tail_position = self.head_movement[-2]
             self.tail_movement.append(self.tail_position)
 
-    def movement(self, movement, moves=0):
+    def body_move(self):
+        ''' move body when head distance >= 2'''
+
+        # let's make first from head
+        if self.head_tail_distance(self.head_position, self.body_positions[0]) >= 2:
+            # go to the same position where head was 1 step ago
+            self.body_positions[0] = self.head_movement[-2]
+            self.body_movements[0].append(self.body_positions[0])
+
+        # and rest
+        for body in range(1, len(self.body_positions)):
+            print(
+                f"body :{body} {self.body_positions[body-1]} {self.body_positions[body]}")
+            if self.head_tail_distance(self.body_positions[body-1], self.body_positions[body]) >= 2:
+                # go to the same position where previous body part was 1 step ago
+                self.body_positions[body] = self.body_movements[body-1][-2]
+                self.body_movements[body].append(self.body_positions[body])
+
+    def movement_part1(self, movement, moves=0):
         ''' moves head and tail
-            movement - list of dictionaries
+            movement - list of lists
             moves - if not defined going throu all steps
         '''
         steps = len(movement) if moves == 0 else moves
         for i in range(steps):
-            for direction, distance in movement[i].items():
-                if direction == 'L':
-                    step = self.head_move_left
-                elif direction == 'R':
-                    step = self.head_move_right
-                elif direction == 'U':
-                    step = self.head_move_up
-                elif direction == 'D':
-                    step = self.head_move_down
+            direction, distance = movement[i]
+            if direction == 'L':
+                step = self.head_move_left
+            elif direction == 'R':
+                step = self.head_move_right
+            elif direction == 'U':
+                step = self.head_move_up
+            elif direction == 'D':
+                step = self.head_move_down
 
-                for i in range(distance):
-                    step()  # head move
-                    self.tail_move()  # tail move
+            for j in range(distance):
+                step()  # head move
+                self.tail_move()  # tail move
 
-    def head_tail_distance(self) -> int:
+    def movement_part2(self, movement, moves=0):
+        ''' moves head and tail
+            movement - list of lists
+            moves - if not defined going throu all steps
+        '''
+
+        steps = len(movement) if moves == 0 else moves
+        for i in range(steps):
+            direction, distance = movement[i]
+            if direction == 'L':
+                step = self.head_move_left
+            elif direction == 'R':
+                step = self.head_move_right
+            elif direction == 'U':
+                step = self.head_move_up
+            elif direction == 'D':
+                step = self.head_move_down
+
+            print(f'going to move : {movement[i]}')
+            for j in range(distance):
+                step()  # head move
+                self.body_move()  # body move
+
+    def head_tail_distance(self, head, tail) -> int:
         '''return max distance of a tail from head
             head [2,3] and tail [1,1] will return 2  (y)
             head [2,3] and tail [-3,1] will return 5 (x)
         '''
-        head_x, head_y = self.head_position
-        tail_x, tail_y = self.tail_position
+        head_x, head_y = head
+        tail_x, tail_y = tail
         # calculate distance
         x = abs(max(head_x, tail_x)-min(head_x, tail_x))
         y = abs(max(head_y, tail_y)-min(head_y, tail_y))
@@ -76,11 +127,21 @@ class Worm:
         # convert to set
         return len(set(self.tail_movement))
 
+    def body_visited(self) -> int:
+        ''' return number of unique visited position for tail'''
+        # convert to set
+        print(self.body_movements[len(self.body_positions)-1])
+        print("\n")
+        print(set(self.body_movements[len(self.body_positions)-1]))
+        return len(set(self.body_movements[len(self.body_positions)-1]))
+
     def __str__(self) -> str:
         return f'Head:{self.head_position}, Tail{self.tail_position}'
 
     def show_field(self) -> str:
+        # TODO later
         pass
 
     def show_visit(self) -> str:
+        # TODO later
         pass
