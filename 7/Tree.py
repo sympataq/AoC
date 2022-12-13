85d5982d4bc91b08c6037371156533f774b11e04
sympataq <52965903+sympataq@users.noreply.github.com>
2022-12-13 13:29:10 +0100

    update

--- 7/Tree.py
+++ 7/Tree.py
@@ -3,9 +3,17 @@ from Node import Node
 
 class Tree
 
-   def __init__(self, root: Node):
-        self.root = root
+   def __init__(self):
+        self.root = Node('root')
+        self.act_dir = '/'
+        self.act_path = '/'
+
+
+
 
     def traverse(self, root='root') -> str:
         pass
+
+    
+
 # ├└──
