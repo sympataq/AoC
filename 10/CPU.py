aef6cc536380b198cbf32f5036127b24bdc67bde
sympataq <52965903+sympataq@users.noreply.github.com>
2022-12-14 05:05:22 +0100

    update

--- 10/CPU.py
+++ 10/CPU.py
@@ -1,27 +1,26 @@
 from collections import deque
 
 
-class CPU:
+class Computer:
 
-    CPU_COMMANDS = {'noop': 1, 'addx': 2}
+    CPU = {'Start': 0, 'Busy': False, 'CMD': {'noop': 1, 'addx': 2}}
     SIGNAL_BOOSTS = [20, 60, 100, 140, 180, 220]
 
     def __init__(self):
         self.cycle = 0
         self.register = 1
-        self.busy = False
 
     def load_program(self, commands):
         self.commands = deque(commands)
 
     def execute_command(self, command, cycle) -> bool:
-
+        # if not self.busy
         pass
 
     def run(self):
         while self.commands:
-            if not self.busy:
-                command = self.commands.popleft()
+            # if not self.busy:
+            command = self.commands.popleft()
             if self.execute_command(self.cycle, command):
                 self.cycle += 1
 
