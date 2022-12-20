44fde73fb6850b82def314434d8fa68b703fbb72
sympataq <52965903+sympataq@users.noreply.github.com>
2022-12-20 19:01:28 +0100

    skro

--- 11/11.py
+++ 11/11.py
@@ -1,13 +1,20 @@
 from Monkey import Monkey
 from Game import Game
 
-
 def read_file(filename):
     file_data = []
     with open(filename) as f:
         for line in f:
-            line = line.r
+            line = line.rstrip()
+            line = line.replace(',','')
+            if line:
+                file_data.append(line)
+    return file_data                
+
+def fill_data(data):
+    pass
 
 
 if __name__ == '__main__':
     file_data = read_file("11/input.txt")
+    g = Game(file_data)
--- 11/Monkey.py
+++ 11/Monkey.py
@@ -1,34 +1,76 @@
 
-
+import math
 from collections import deque
 
 
 class Monkey:
 
-    def test(self, denominator: int) -> int:
-        return 0
+    def worry_check(self, item) -> int:
+        return self.operation(item)
+
+    def div_check(self, num):
+        return True if num % self.divisible == 0 else False
+
+    def round(self):
+        for i in range(len(self.items)):
+            item = self.items.popleft()
+            item = math.floor((self.worry_check(item)) / 3)
+            if (self.div_check(item)):
+                self.positive_throw.throw(item)
+            else:
+                self.negative_throw.throw(item)
+
+    def receive(self, item):
+        self.items.append(item)
+
+    def negative_throw_init(self, monkey):
+        self.negative_throw = monkey
+
+    def positive_throw_init(self, monkey):
+        self.positive_throw = monkey
 
     def operation_mul(self, first, second) -> int:
-        return 0
+        a = first if not self.first_old else self.first
+        b = second if not self.second_old else self.second
+        return a * b
 
     def operation_plus(self, first, second) -> int:
-        return 0
+        a = first if not self.first_old else self.first
+        b = second if not self.second_old else self.second
+        return a + b
 
-    def operation(self):
-        if self.operation == '*':
-            pass
-        elif self.operation == '+':
-            pass
-
-        pass
 
     def operation_init(self, operation, first, second):
-        self.operation = operation
-        self.first = first
-        self. second = second
-        pass
+        self.operation_sign = operation
+        if self.operation_sign == '*':
+            self.operation = self.operation_mul
+        elif self.operation_sign == '+':
+            self.operation = self.operation_plus
 
-    def __init__(self, items, divisible, monkey_throw) -> None:
+        if first == 'old':
+            self.first_old = True
+        else:
+            self.first_old = False
+
+        if second == 'old':
+            self.second_old = True
+        else:
+            self.second_old = False    
+        print(self.items)
+        self.first = int(first) if not self.first_old else self.items[0]
+        self.second = int(second) if not self.second_old else self.items[0]
+
+    def __init__(self, name, items, divisible) -> None:
+        self.name = name
         self.items = deque(items)
         self.divisible = divisible
-        self.monkey_throw = monkey_throw
+
+    def __str__(self) -> str:
+        ret = ''
+        ret += f'{self.name}:'
+        ret += f'  Starting items:{self.name}:'
+        ret += f'  Operation: new = {self.first} {self.operation_sign} {self.second}:'
+        ret += f'  Test : divisable by {self.divisible}'
+        ret += f'    If true: throw to monkey {self.positive_throw}:'
+        ret += f'    If false: throw to monkey{self.negative_throw}:'
+        return ret
